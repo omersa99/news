7b8da92d5262b9d552eb66b29a6258c95679f13a
OmarSabbah999 <omars@mail.tau.ac.il>
2022-07-05 22:30:26 +0300

    react changes

---
+++ components/Newsitem.js
@@ -0,0 +1,12 @@
+
+import * from '../ui/newsitem';
+
+function Newsitem(props){
+    return(
+    <li>
+        <div className={ui.title}">title</div>
+    </li>
+    );
+}
+
+export default Newsitem;
